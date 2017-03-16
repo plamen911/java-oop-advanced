38a45b8f2fd91b49deff22a299fb9065491778f1
apple <1>
2017-03-16 14:38:40 +0200

    Create 01. Define an Interface Person

--- src/_01InterfacesAndAbstractionExercises/_01DefineAnInterfacePerson/Person.java
+++ src/_01InterfacesAndAbstractionExercises/_01DefineAnInterfacePerson/Person.java
@@ -4,9 +4,6 @@ package _01InterfacesAndAbstractionExercises._01DefineAnInterfacePerson;
  * Created by Plamen Markov on 3/14/17.
  */
 public interface Person {
-    String name = null;
-    int age = 0;
-
     String getName();
     int getAge();
 }
