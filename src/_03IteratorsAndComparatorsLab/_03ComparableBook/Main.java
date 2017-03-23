c5b36ff8bf79db4e07f497516079d3e7f5b712ba
apple <1>
2017-03-23 16:35:14 +0200

    Create ComparableBook

---
+++ src/_03IteratorsAndComparatorsLab/_03ComparableBook/Main.java
@@ -0,0 +1,21 @@
+// https://judge.softuni.bg/Contests/Practice/Index/523#2
+package _03IteratorsAndComparatorsLab._03ComparableBook;
+
+/**
+ * Created by Plamen Markov on 3/23/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        if (bookOne.compareTo(bookTwo) > 0) {
+            System.out.println(String.format("%s is before %s", bookOne, bookTwo));
+        } else if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.println(String.format("%s is before %s", bookTwo, bookOne));
+        } else {
+            System.out.println("Book are equal");
+        }
+    }
+}
