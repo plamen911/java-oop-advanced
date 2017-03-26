8e76d7e812d1190e238940ec8b40f33db11485d9
apple <1>
2017-03-26 10:26:45 +0300

    Create 02. Card Rank

---
+++ src/_04EnumerationsAndAnnotationsExercises/_02CardRank/CardRanks.java
@@ -0,0 +1,8 @@
+package _04EnumerationsAndAnnotationsExercises._02CardRank;
+
+/**
+ * Created by Plamen Markov on 3/26/17.
+ */
+public enum CardRanks {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+}
--- src/_04EnumerationsAndAnnotationsExercises/_02CardRank/Main.java
+++ src/_04EnumerationsAndAnnotationsExercises/_02CardRank/Main.java
@@ -1,7 +1,14 @@
+// https://judge.softuni.bg/Contests/Practice/Index/251#1
 package _04EnumerationsAndAnnotationsExercises._02CardRank;
 
 /**
  * Created by Plamen Markov on 3/26/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Ranks:");
+        for (CardRanks cardRanks : CardRanks.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardRanks.ordinal(), cardRanks);
+        }
+    }
 }
